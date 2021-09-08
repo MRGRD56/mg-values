a061433eae226684d845471764572e1a0d3600ec
MRGRD56 <kirgol85@gmail.com>
2021-09-08 10:06:28 +0500

    v1.1.0 improve classes() function, add more tests

--- index.ts
+++ index.ts
@@ -1,27 +1,39 @@
 type BoolExp = boolean | (() => boolean);
 
-export function vif<TResult>(expression: BoolExp, value: TResult, alternativeValue: TResult | string = ""): TResult | string {
-    const exp = typeof expression === "function"
-        ? expression()
-        : expression;
+function getBoolExpValue(boolExp: BoolExp): boolean {
+    return typeof boolExp === "function"
+        ? boolExp()
+        : boolExp;
+}
 
+export function vif<TResult>(expression: BoolExp, value: TResult, alternativeValue: TResult | string = ""): TResult | string {
+    const exp = getBoolExpValue(expression);
     return exp ? value : alternativeValue;
 }
 
-export function classes(classesObj: Record<string, BoolExp>): string {
-    return Object.keys(classesObj)
-        .filter(key => Object.prototype.hasOwnProperty.call(classesObj, key))
+export function classes(classesObj: Record<string, BoolExp>, requiredClasses: string[] | string = undefined): string {
+    let reqClassesString = "";
+    if (requiredClasses !== undefined) {
+        reqClassesString = Array.isArray(requiredClasses)
+            ? (<string[]>requiredClasses).map(c => c.trim()).join(" ")
+            : <string>requiredClasses;
+        reqClassesString = reqClassesString.trim();
+        if (reqClassesString) {
+            reqClassesString += " ";
+        }
+    }
+
+    const classesByPredicates = Object.keys(classesObj)
+        .map(key => key?.trim())
+        .filter(key => key && Object.prototype.hasOwnProperty.call(classesObj, key))
         .map(key => {
             return {
                 key: key,
                 value: classesObj[key]
             };
         })
-        .filter(kv => {
-            return (typeof kv.value) === "function"
-                ? (<() => boolean>kv.value)()
-                : kv;
-        })
-        .map(kv => kv.key)
+        .filter(kv => getBoolExpValue(kv.value))
+        .map(kv => kv.key.trim())
         .join(" ");
+    return reqClassesString + classesByPredicates;
 }
--- tests.test.ts
+++ tests.test.ts
@@ -1,23 +1,32 @@
 import {vif, classes} from "./index";
 
-function testEquality(testName: string, expected: any, actual: any) {
-    test(testName, () => {
-        expect(actual).toBe(expected);
-    });
-}
-
-test("vif test 1", () => {
+test("vif test 1.1", () => {
     const actual = vif(() => 2 > 1, "YES");
     expect(actual).toBe("YES");
 });
 
-test("vif test 2", () => {
+test("vif test 1.2", () => {
+    const actual = vif(2 > 1, "YES");
+    expect(actual).toBe("YES");
+});
+
+test("vif test 2.1", () => {
+    const actual = vif(() => 1 > 2, 1, 5);
+    expect(actual).toBe(5);
+});
+
+test("vif test 2.2", () => {
     const actual = vif(1 > 2, 1, 5);
     expect(actual).toBe(5);
 });
 
-test("vif test 3", () => {
+test("vif test 3.1", () => {
     const actual = vif(() => 1 > 2, { isYes: true });
+    expect(actual).toBe("");
+});
+
+test("vif test 3.2", () => {
+    const actual = vif(1 > 2, { isYes: true });
     expect(actual).toBe("");
 });
 
@@ -30,7 +39,25 @@ test("classes test 1", () => {
     expect(actual).toBe("red blue");
 });
 
-test("classes test empty", () => {
+test("classes test 2 empty", () => {
     const actual = classes({});
     expect(actual).toBe("");
+});
+
+test("classes test 3 with second parameter, string", () => {
+    const actual = classes({
+        "red": true,
+        "green": false,
+        "blue": 1 === 1
+    }, "d-flex");
+    expect(actual).toBe("d-flex red blue");
+});
+
+test("classes test 4 with second parameter, string[]", () => {
+    const actual = classes({
+        "red": true,
+        "green": () => false,
+        "blue": () => 1 === 1
+    }, ["my-element", "mb-3"]);
+    expect(actual).toBe("my-element mb-3 red blue");
 });
